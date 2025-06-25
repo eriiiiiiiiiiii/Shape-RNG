13018fdf4c3e88b50f43b8003a6c4db42bb4b5a1
eriiiiiiiiiiii <eric.a.burdan@gmail.com>
2025-06-25 15:06:34 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.getElementById('triangleCanvas');
-  const ctx = canvas.getContext('2d');
+import { showTriangle } from './fadeAnimation.js';
 
-  function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
+const container = document.getElementById('container');
+const button = document.getElementById('spawnBtn');
 
-  resizeCanvas();
-  window.addEventListener('resize', resizeCanvas);
-
-  function drawTriangle() {
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
-
-    const size = 100;
-    ctx.fillStyle = 'white';
-    ctx.beginPath();
-    ctx.moveTo(centerX, centerY - size / Math.sqrt(3));
-    ctx.lineTo(centerX - size / 2, centerY + size / (2 * Math.sqrt(3)));
-    ctx.lineTo(centerX + size / 2, centerY + size / (2 * Math.sqrt(3)));
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  document.getElementById('spawnBtn').addEventListener('click', drawTriangle);
+button.addEventListener('click', () => {
+  showTriangle(container);
 });
